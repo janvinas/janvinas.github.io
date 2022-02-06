0e712055a94ca6ea6f2397f0ae037318c885ecdc
Jan Viñas <mirabetjan@gmail.com>
2022-02-06 11:36:56 +0100

    afegir icones de les línies

--- mapa/main.js
+++ mapa/main.js
@@ -16,6 +16,39 @@ map.fitBounds(bounds);
 
 var trainsLayer = L.layerGroup().addTo(map);
 
+
+var icon446 = L.icon({
+    iconUrl: 'icons/es446v.png',
+    
+    iconSize: [40, 48],
+    iconAnchor: [20, 24],
+    popupAnchor: [0, 0]
+});
+
+var trainIcon = L.Icon.extend({
+    options:{
+        iconSize: [40, 48],
+        iconAnchor: [20, 24],
+        popupAnchor: [0, -20]
+    }
+});
+
+function getIcon(trainName){
+    if(trainName.includes('R1') || trainName.includes('R4') ||trainName.includes('R8')){
+        return new trainIcon({iconUrl: 'icons/es446v.png'});
+    }else if(trainName.includes('S1')){
+        return new trainIcon({iconUrl: 'icons/es-fgc113.png'}); //no existeix la serie 115
+    }else if(trainName.includes('S2')){
+        return new trainIcon({iconUrl: 'icons/es-fgc213.png'});
+    }else if(trainName.includes('R2')){
+        return new trainIcon({iconUrl: 'icons/es450v.png'});
+    }else if(trainName.includes('Euromed')){
+        return new trainIcon({iconUrl: 'icons/es130.png'});
+    }else{
+        return new L.Icon.Default();
+    }
+};
+
 function clearMap(){
     trainsLayer.clearLayers();
 }
@@ -34,7 +67,8 @@ function updateMap(){
             var coordx = traincoords.substring(traincoords.indexOf(":") + 1, traincoords.indexOf(","));
             var coordz = traincoords.substring(traincoords.lastIndexOf(",") + 1, traincoords.length)
             
-            var marker = L.marker([convertRange(coordz, [-3106, 4576], [4576, -3106]), coordx]).bindPopup(trainname).addTo(trainsLayer);
+            var trainIcon = getIcon(trainname);
+            var marker = L.marker([convertRange(coordz, [-3106, 4576], [4576, -3106]), coordx], {icon: trainIcon}).bindPopup(trainname).addTo(trainsLayer);
             if(trainname == currentPopup) marker.openPopup();
         }
     }
